bbce44d17f15e52604d75aee4c4fdd4fe27ec980
Supachock Taenjetriw <ampandj@gmail.com>
2024-02-16 14:57:08 +0700

    review gun login google

--- my-project/src/login/components/GoogleButton.jsx
+++ my-project/src/login/components/GoogleButton.jsx
@@ -1,52 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
-import { gapi } from 'gapi-script';
+const clientId = "902199029700-1pv3edc1p4oqe8kj2k77ltqapea1ooa2.apps.googleusercontent.com";
 
-const GoogleButton = () => {
-    const clientId = "902199029700-1pv3edc1p4oqe8kj2k77ltqapea1ooa2.apps.googleusercontent.com";
+export default function GoogleButton() {
     const [profile, setProfile] = useState(null);
 
     useEffect(() => {
-        const initClient = () => {
+        gapi.load("client:auth2", () => {
             gapi.client.init({
                 clientId: clientId,
                 scope: ''
+            }).then(() => {
+                console.log('Google API initialized successfully');
+            }).catch((error) => {
+                console.error('Error initializing Google API:', error);
             });
-        };
-
-        gapi.load("client:auth2", initClient);
+        });
     }, []);
-
-    const success = (res) => {
-        console.log('success', res);
-        setProfile(res.profileObj); // Assuming profileObj contains user profile information
+    
+    const handleSuccess = (res) => {
+        setProfile(res.profileObj);
     };
 
-    const failure = (res) => {
-        console.log('failure', res);
+    const handleFailure = (res) => {
+        alert('Login failure:', res)
     };
-    const logout =  () => {
-        setProfile(null);
-    }
+
     return (
         <div>
-            <br /><br />
             {profile ? (
                 <div>
-                    <img src = {profile.imageUrl} alt = "user image" />
+                    <img src={profile.imageUrl} alt="user image" />
                     <h2>Login success</h2>
-                    {/* Display user profile information here */}
                     <p>Name: {profile.name}</p>
                     <p>Email: {profile.email}</p>
-                    <GoogleLogout clientId={clientId} buttonText='Log out'  onLogoutSuccess={logout}/>
-                    {/* Add more profile information as needed */}
+                    <GoogleLogout clientId={clientId} buttonText='Log out' onLogoutSuccess={() => setProfile(null)} />
                 </div>
             ) : (
                 <GoogleLogin
                     clientId={clientId}
                     buttonText='Sign in with Google'
-                    onSuccess={success}
-                    onFailure={failure}
+                    onSuccess={handleSuccess}
+                    onFailure={handleFailure}
                     cookiePolicy={'single_host_origin'}
                     isSignedIn={true}
                 />
@@ -55,4 +48,6 @@ const GoogleButton = () => {
     );
 };
 
-export default GoogleButton;
+import { useEffect, useState } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { gapi } from 'gapi-script';
