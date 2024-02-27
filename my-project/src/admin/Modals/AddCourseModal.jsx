3c299cc8db27236df837d8afb7e3da6807ab1ac1
skymarshell <rungradis15946@gmail.com>
2024-02-27 23:21:17 +0700

    respovesive table

--- my-project/src/admin/Modals/AddCourseModal.jsx
+++ my-project/src/admin/Modals/AddCourseModal.jsx
@@ -1,159 +1,176 @@
-import { useState } from 'react';
-import { createPortal } from 'react-dom';
-import Papa from 'papaparse'; // Library for parsing CSV files
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import Papa from "papaparse"; // Library for parsing CSV files
 
-import Table from '../components/Table';
-import TableRow from '../components/TableRow';
-import AddCourseSideBar from '../components/AddCourseSideBar';
+import Table from "../components/Table";
+import TableRow from "../components/TableRow";
+import AddCourseSideBar from "../components/AddCourseSideBar";
 
-import transferIcon from '../../assets/transfer.png'
-import Manualimport from '../components/ManualImport';
-import { useImportCourseMutation } from '../../context/Admin-Context';
+import transferIcon from "../../assets/transfer.png";
+import Manualimport from "../components/ManualImport";
+import { useImportCourseMutation } from "../../context/Admin-Context";
 
 const generateOptions = () => {
-    const options = [];
-    for (let i = 0; i < 100; i++) {
-        const value = i.toString().padStart(2, '0');
-        options.push(
-            <option key={i} value={value}>
-                {value}
-            </option>
-        );
-    }
-    return options;
+  const options = [];
+  for (let i = 0; i < 100; i++) {
+    const value = i.toString().padStart(2, "0");
+    options.push(
+      <option key={i} value={value}>
+        {value}
+      </option>
+    );
+  }
+  return options;
 };
 
-export default function AddCourseModal({ courseTag, branchTag, isVisible, onClose }) {
-    const [selectedCurriculum, setSelectedCurriculum] = useState('65');
-    const [importedData, setImportedData] = useState([]);
-    const [filteredData, setFilteredData] = useState([]);
-    const options = generateOptions();
+export default function AddCourseModal({
+  courseTag,
+  branchTag,
+  isVisible,
+  onClose,
+}) {
+  const [selectedCurriculum, setSelectedCurriculum] = useState("65");
+  const [importedData, setImportedData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const options = generateOptions();
 
-    const mutation = useImportCourseMutation();
-    const handleImportDatabase = () => mutation.mutate(filteredData);
+  const mutation = useImportCourseMutation();
+  const handleImportDatabase = () => mutation.mutate(filteredData);
 
-    const handleCurriculumChange = (e) => {
-        setSelectedCurriculum(e.target.value);
-    }
+  const handleCurriculumChange = (e) => {
+    setSelectedCurriculum(e.target.value);
+  };
 
-    const handleImport = (event) => {
-        const file = event.target.files[0];
-        const reader = new FileReader();
+  const handleImport = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
 
-        reader.onload = handleFileLoad;
-        reader.readAsText(file);
-    };
+    reader.onload = handleFileLoad;
+    reader.readAsText(file);
+  };
 
-    const handleFileLoad = (e) => {
-        const text = e.target.result;
-        const result = Papa.parse(text, { header: true }); // Parsing CSV data with headers
+  const handleFileLoad = (e) => {
+    const text = e.target.result;
+    const result = Papa.parse(text, { header: true }); // Parsing CSV data with headers
 
-        const dataWithTags = result.data.map(item => ({
-            ...item,
-            branch_tag: branchTag,
-            course_tag: courseTag,
-            curriculum: selectedCurriculum
-        }));
+    const dataWithTags = result.data.map((item) => ({
+      ...item,
+      branch_tag: branchTag,
+      course_tag: courseTag,
+      curriculum: selectedCurriculum,
+    }));
 
-        setImportedData(dataWithTags);
-    };
+    setImportedData(dataWithTags);
+  };
 
-    const filterDataByCourseTag = () => {
-        const filtered = importedData.filter(row => row.course_code.startsWith(courseTag));
-        setFilteredData(filtered);
+  const filterDataByCourseTag = () => {
+    const filtered = importedData.filter((row) =>
+      row.course_code.startsWith(courseTag)
+    );
+    setFilteredData(filtered);
 
-        const remainingData = importedData.filter(row => !row.course_code.startsWith(courseTag));
-        setImportedData(remainingData);
-    };
+    const remainingData = importedData.filter(
+      (row) => !row.course_code.startsWith(courseTag)
+    );
+    setImportedData(remainingData);
+  };
 
-    const handleTransfer = (rowData) => {
-        setFilteredData(prevFilteredData => [...prevFilteredData, rowData]);
-        setImportedData(prevImportedData => prevImportedData.filter(item => item === rowData));
-    };
+  const handleTransfer = (rowData) => {
+    setFilteredData((prevFilteredData) => [...prevFilteredData, rowData]);
+    setImportedData((prevImportedData) =>
+      prevImportedData.filter((item) => item === rowData)
+    );
+  };
 
-    return isVisible && <PortalContainer>
+  return (
+    isVisible && (
+      <PortalContainer>
         <AddCourseSideBar
-            options={options}
-            courseTag={courseTag}
-            selectedCurriculum={selectedCurriculum}
-            handleCurriculumChange={handleCurriculumChange}
-            handleImport={handleImport}
-            onClose={onClose}
+          options={options}
+          courseTag={courseTag}
+          selectedCurriculum={selectedCurriculum}
+          handleCurriculumChange={handleCurriculumChange}
+          handleImport={handleImport}
+          onClose={onClose}
         />
-        
-        <div className='w-full flex flex-col rounded-lg bg-slate-400'>
-            <Table bg={'bg-orange-200'} text='Validation table'>
-                <tbody className="divide-y bg-white divide-gray-200">
-                    {importedData.map((rowData, index) => (
-                        <TableRow key={index}
-                            courseCode={rowData.course_code}
-                            defaultCurriculum={selectedCurriculum}
-                            thName={rowData.th_name}
-                            engName={rowData.eng_name}
-                            credits={rowData.credit}
-                            courseType={rowData.course_type}
-                            rowData={rowData}
-                            onTransfer={handleTransfer}
-                            options={options}
-                        />
-                    ))}
-                </tbody>
-            </Table>
 
-            <TableValidation filterDataByCourseTag={filterDataByCourseTag} />
+        <div className="w-full flex flex-col rounded-lg bg-slate-400">
+          <Table bg={"bg-orange-200"} text="Validation table">
+            <tbody className="divide-y bg-white divide-gray-200">
+              {importedData.map((rowData, index) => (
+                <TableRow
+                  key={index}
+                  courseCode={rowData.course_code}
+                  defaultCurriculum={selectedCurriculum}
+                  thName={rowData.th_name}
+                  engName={rowData.eng_name}
+                  credits={rowData.credit}
+                  courseType={rowData.course_type}
+                  rowData={rowData}
+                  onTransfer={handleTransfer}
+                  options={options}
+                />
+              ))}
+            </tbody>
+          </Table>
 
-            <Table bg='bg-emerald-200' text='Import table'>
-                <tbody className='divide-y bg-white divide-gray-200'>
-                    {filteredData.map((rowData, index) => (
-                        <TableRow
-                            key={index}
-                            courseCode={rowData.course_code}
-                            defaultCurriculum={rowData.curriculum}
-                            thName={rowData.th_name}
-                            engName={rowData.eng_name}
-                            credits={rowData.credit}
-                            courseType={rowData.course_type}
-                            rowData={rowData}
-                            onTransfer={handleTransfer}
-                            options={options}
-                        />
-                    ))}
-                </tbody>
-            </Table>
+          <TableValidation filterDataByCourseTag={filterDataByCourseTag} />
 
-            <TableImportButton handleImportDatabase={handleImportDatabase} />
-            <Manualimport />
+          <Table bg="bg-emerald-200" text="Import table">
+            <tbody className="divide-y bg-white divide-gray-200">
+              {filteredData.map((rowData, index) => (
+                <TableRow
+                  key={index}
+                  courseCode={rowData.course_code}
+                  defaultCurriculum={rowData.curriculum}
+                  thName={rowData.th_name}
+                  engName={rowData.eng_name}
+                  credits={rowData.credit}
+                  courseType={rowData.course_type}
+                  rowData={rowData}
+                  onTransfer={handleTransfer}
+                  options={options}
+                />
+              ))}
+            </tbody>
+          </Table>
+
+          <TableImportButton handleImportDatabase={handleImportDatabase} />
+          {/* ----------Manualimport--------------- */}
+          <Manualimport />
         </div>
-    </PortalContainer>
+      </PortalContainer>
+    )
+  );
 }
 
 const PortalContainer = ({ children }) => {
-    return (
-        createPortal(
-            <div className='w-screen h-screen fixed flex bg-gray-800 bg-opacity-50 z-[10]'>
-                {children}
-            </div>,
-            document.getElementById('root-modal')
-        )
-    )
-}
+  return createPortal(
+    <div className="w-screen h-screen fixed flex bg-gray-800 bg-opacity-50 z-[10]">
+      {children}
+    </div>,
+    document.getElementById("root-modal")
+  );
+};
 
 function TableImportButton({ handleImportDatabase }) {
-    return (
-        <div className='flex justify-center my-4'>
-            <button className='font-bold text-white rounded bg-green-500 hover:bg-green-700 py-2 px-6 ' onClick={handleImportDatabase}>
-                Import to Database
-            </button>
-        </div>
-    );
+  return (
+    <div className="flex justify-center my-4">
+      <button
+        className="font-bold text-white rounded bg-green-500 hover:bg-green-700 py-2 px-6 "
+        onClick={handleImportDatabase}>
+        Import to Database
+      </button>
+    </div>
+  );
 }
 
 function TableValidation({ filterDataByCourseTag }) {
-    return (
-        <div className='flex justify-center my-10'>
-            <button className='inline-block' onClick={filterDataByCourseTag}>
-                <img src={transferIcon} alt="transfer icon" className='h-16' />
-            </button>
-        </div>
-    );
+  return (
+    <div className="flex justify-center my-10">
+      <button className="inline-block" onClick={filterDataByCourseTag}>
+        <img src={transferIcon} alt="transfer icon" className="h-16" />
+      </button>
+    </div>
+  );
 }
--- my-project/src/admin/components/ManualImport.jsx
+++ my-project/src/admin/components/ManualImport.jsx
@@ -1,179 +1,77 @@
 import React from "react";
 import { useState } from "react";
 
+const BtnCloseMenu = () => (
+  <div id="manualImport">
+    <button onClick={closeManual}>Back</button>
+  </div>
+);
+
+const TableImport = () => (
+  <table className="">
+    <thead>
+      <tr>
+        <th>Course Code</th>
+        <th>Curriculum</th>
+        <th>TH name</th>
+        <th>Eng name</th>
+        <th>Credits</th>
+        <th>Course type</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <input type="number" name="" id="" />
+        </td>
+        <td>
+          <select name="" id="">
+            <option value="">1</option>
+            <option value="">2</option>
+            <option value="">3</option>
+          </select>
+        </td>
+        <td>
+          <input type="text" name="" id="" placeholder="th" />
+        </td>
+        <td>
+          <input type="text" name="" id="" placeholder="eng" />
+        </td>
+        <td>
+          <input type="text" name="" id="" placeholder="credit" />
+        </td>
+        <td>
+          <select name="" id="">
+            <option value="เฉพาะทั่วไป">เฉพาะทั่วไป</option>
+            <option value="เฉพาะเลือก">เฉพาะเลือก</option>
+          </select>
+        </td>
+        <td>
+          <button>Delete</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+);
+
+function closeManual() {
+  let memu = document.getElementById("manualImport");
+  memu.classList.toggle("invisible");
+}
+
 function Manualimport() {
-  const [isTableVisible, setIsTableVisible] = useState(false);
-  const [rows, setRows] = useState([]);
-  const [rowCount, setRowCount] = useState(0);
-
-  const toggleTable = () => {
-    setIsTableVisible(!isTableVisible);
-  };
-
-  const addRow = () => {
-    const newRow = {
-      courseCode: "",
-      curriculum: "",
-      thName: "",
-      enName: "",
-      credits: "",
-      courseType: "",
-    };
-    setRowCount(rowCount + 1);
-    setRows([...rows, newRow]);
-  };
-
-  const deleteRow = (index) => {
-    const updatedRows = [...rows];
-    updatedRows.splice(index, 1);
-    setRowCount(rowCount - 1);
-    setRows(updatedRows);
-  };
-
-  console.log();
-
-  function closeManual() {
-    let memu = document.getElementById("manualImport");
-    memu.classList.add("invisible");
-  }
-  /*function btnC() {
-    // Access all elements with the class 'code'
-    const inputs = document.querySelectorAll('.code');   
-    // Create an array to store the values
-    const values = [];   
-    // Loop through each input element
-    inputs.forEach(input => {
-        // Push the value of each input to the 'values' array
-        values.push(input.value);
-    });
-    
-    console.log(values); // Output the array of values
-    <button onClick={btnC}>submit</button>
-    
-  }*/
-
   return (
-    <div
-      id="manualImport"
-      className="absolute top-0 left-0 w-full min-h-dvh flex flex-col bg-white invisible">
-      <button onClick={closeManual}>Back</button>
-
-      <br />
-
-      <br />
-      <button onClick={toggleTable}>
-        {isTableVisible ? "Hide Table" : "Show Table"}
-      </button>
-      {isTableVisible && (
-        <div
-          id="table-container"
-          style={{
-            backgroundColor: "lightgray",
-            padding: "10px",
-            marginTop: "10px",
-          }}>
-          <form action="">
-            <table className="table-auto">
-              <thead>
-                <tr>
-                  <th style={{ backgroundColor: "green", color: "white" }}>
-                    course code
-                  </th>
-                  <th style={{ backgroundColor: "green", color: "white" }}>
-                    curriculum
-                  </th>
-                  <th style={{ backgroundColor: "green", color: "white" }}>
-                    th-name
-                  </th>
-                  <th style={{ backgroundColor: "green", color: "white" }}>
-                    en-name
-                  </th>
-                  <th style={{ backgroundColor: "green", color: "white" }}>
-                    credits
-                  </th>
-                  <th style={{ backgroundColor: "green", color: "white" }}>
-                    course type
-                  </th>
-                </tr>
-              </thead>
-              <tbody id="table-body">
-                {rows.map((row, index) => (
-                  <tr key={index}>
-                    <td>
-                      <input
-                        class="code"
-                        placeholder="0360xxxx"
-                        type="text"
-                        maxLength="8"
-                        pattern="\d{8}"
-                        size="10"
-                      />
-                    </td>
-                    <td>
-                      <select className="curriculum">
-                        {Array.from({ length: 100 }, (_, index) => {
-                          const value = index.toString().padStart(2, "0");
-                          return (
-                            <option key={index} value={value}>
-                              {value}
-                            </option>
-                          );
-                        })}
-                      </select>
-                    </td>
-                    <td>
-                      <input class="th-name" type="text" size="60" />
-                    </td>
-                    <td>
-                      <input class="en-name" type="text" size="60" />
-                    </td>
-                    <td>
-                      <select className="credits">
-                        {Array.from({ length: 4 }, (_, index) => {
-                          const value = (index + 1).toString();
-                          return (
-                            <option key={index} value={value}>
-                              {value}
-                            </option>
-                          );
-                        })}
-                      </select>
-                    </td>
-                    <td>
-                      <select className="type">
-                        <option value="เฉพาะบังคับ">เฉพาะบังคับ</option>
-                        <option value="เฉพาะเลือก">เฉพาะเลือก</option>
-                      </select>
-                    </td>
-                    <td>
-                      <button
-                        className="delete-button"
-                        style={{ backgroundColor: "red", color: "black" }}
-                        onClick={() => deleteRow(index)}>
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </form>
-          <button
-            style={{
-              backgroundColor: "lime",
-              color: "black",
-              marginRight: "5px",
-            }}
-            onClick={addRow}
-            disabled={rowCount === 5 ? true : false}>
-            Add Row
-          </button>
-          <button style={{ backgroundColor: "lime", color: "black" }}>
-            submit
-          </button>
+    <>
+      <div
+        id="manualImport"
+        className=" flex flex-col  bg-black/50 top-0 left-0 absolute w-full min-h-dvh text-white justify-center items-center ">
+        <div className="flex flex-col p-5  bg-white/100 text-black text-center w-2/3 overflow-auto">
+          <BtnCloseMenu />
+          <TableImport />
         </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 }
 
