9ed7fec7a3df973e8f30e15e33f73c01dadc9164
Supachock Taenjetriw <ampandj@gmail.com>
2024-03-14 22:16:42 +0700

    SideBarRight toggle 1

--- my-project/src/prof/layouts/SideBarRight.jsx
+++ my-project/src/prof/layouts/SideBarRight.jsx
@@ -1,50 +1,65 @@
+import { useState } from 'react';
 import { useCoursesContext } from '../../context/Prof-Context';
-import waitingIcon from '../../assets/more.png'
+import waitingIcon from '../../assets/more.png';
+
+const slideDown = 'transition-all ease-in-out duration-300 ';
 
 export default function SideBarRight() {
     const { profCourses } = useCoursesContext();
-    console.log(profCourses)
+    const [isMyGroupsOpen, setIsMyGroupsOpen] = useState(true);
+    const myGroupClass = isMyGroupsOpen ? 'h-dvh' : 'h-0';
+
+    const handleToggleMyGroups = (e) => {
+        e.preventDefault();
+        setIsMyGroupsOpen(prev => !prev);
+    };
+
     return (
-        <div className='overflow-y-auto max-h-dvh grid grid-cols-1 grid-flow-row auto-rows-min col-start-18 col-span-3 items-center border-t-2 border-black bg-ghost_white'>
-            <div className='bg-burnt_sienna/65 p-1'>
+        <div className='col-start-18 col-span-3 border-t-2 border-black bg-ghost_white'>
+            <div className='bg-burnt_sienna/65 hover:bg-burnt_sienna/75 pt-6 pb-5 cursor-pointer' onClick={handleToggleMyGroups} onContextMenu={e => e.preventDefault()} >
                 <p className='sm:text-xl font-semibold text-center text-white'>My Groups</p>
             </div>
-            {profCourses && profCourses.map(course => (
-                <div key={course.id} className='mt-2 border-0 border-t-2 border-black bg-gray-200'>
-                    <div className='flex justify-between'>
-                        <p className='ml-2'>{course.combined_code_curriculum}</p>
-                        <p className='mx-2'>{course.course_type}</p>
-                    </div>
-                    {course.groups.map(group => (
-                        <ProfGroup key={group.id} group={group} />
-                    ))}
-                </div>
-            ))}
+            <div className={'overflow-y-auto flex flex-col ' + slideDown + myGroupClass}>
+                {profCourses && profCourses.map(course => <CourseGroups key={course.id} course={course} />)}
+            </div>
+        </div>
+    );
+}
+
+const CourseGroups = ({ course }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const myCourseClass = isOpen ? 'h-fit' : 'h-0';
+
+    return (
+        <div className={'border-t-2 border-black bg-gray-200'}>
+            <div className='flex justify-between' onClickCapture={() => setIsOpen(prev => !prev)}>
+                <p className='ml-2'>{course.combined_code_curriculum}</p>
+                <p className='mx-2'>{course.course_type}</p>
+            </div>
+            <div className={'overflow-hidden ' + myCourseClass}>
+                {course.groups.map(group => <ProfGroup key={group.id} group={group} />)}
+            </div>
         </div>
     );
 }
 
 const ProfGroup = ({ group }) => {
-    const startTimeParts = group.start_time.split(':');
-    const endTimeParts = group.end_time.split(':');
-    const startTime = `${startTimeParts[0].padStart(2, '0')}:${startTimeParts[1]}`;
-    const endTime = `${endTimeParts[0].padStart(2, '0')}:${endTimeParts[1]}`;
+    const startTime = group.start_time.slice(0, 5);
+    const endTime = group.end_time.slice(0, 5);
 
     return (
         <div className='flex flex-col ring-1 ring-red-300 py-1'>
             <div className='flex justify-between mx-3'>
-                <img src={waitingIcon} className='h-6' />
+                <img src={waitingIcon} alt='Waiting Icon' className='h-6' />
                 <p>{group.num}</p>
                 <p>{group.day_of_week}</p>
                 <p>{startTime}-{endTime}</p>
             </div>
             <div className='flex overflow-x-auto mt-1'>
-                {group.branch_years && group.branch_years.map((branchYear, index) => (
-                    <BranchYearItems key={index} branchYear={branchYear} />
-                ))}
+                {group.branch_years && group.branch_years.map((branchYear, index) => <BranchYearItems key={index} branchYear={branchYear} />)}
             </div>
         </div>
-    )
+    );
 }
 
 const BranchYearItems = ({ branchYear }) => {
